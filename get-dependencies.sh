9e0feb0622eaff87ccba5de33806604041061369
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-16 21:06:57 -0400

    add `wayland`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor pipewire-audio pipewire-jack
+pacman -Syu --noconfirm \
+	libdecor       \
+	pipewire-audio \
+	pipewire-jack  \
+	wayland
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
